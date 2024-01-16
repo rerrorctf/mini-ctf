4ed17ed29838df0e383dc0dde288f439eea89821
smiley <daniel.account@gmail.com>
2024-01-16 19:08:16 +0000

    update solver script

--- rev/solve.py
+++ rev/solve.py
@@ -4,13 +4,10 @@ import subprocess
 
 for combination in product(string.printable, repeat=2):
     flag = "flag{" + "".join(combination) + "}"
-    command = "echo -n " + flag + " | ./task"
+    command = "echo -n \"" + flag + "\" | ./task"
     result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
     if "correct :)" in result.stdout:
-        print(f"potential: {flag}")
+        print(flag)
 
 # $ python3 ./solve.py
-# potential: flag{rd}
-# potential: flag{re}
-# potential: flag{vd}
-# potential: flag{ve}
+# flag{re}

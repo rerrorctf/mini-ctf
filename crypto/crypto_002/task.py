a9e7e9c16e79f2dda18dacab41696bdd96bd28ac
dmur1 <93072266+dmur1@users.noreply.github.com>
2024-05-24 23:06:35 +0100

    implement factorial in a way doesn't crash as that is confusing

--- crypto/crypto_002/task.py
+++ crypto/crypto_002/task.py
@@ -1,11 +1,19 @@
 import secrets
-import math
 import flag
+
+def factorial(x):
+    f = 1
+    for i in range(x):
+        f = f * (i + 1)
+    return f
 
 def make_prime(bits):
     while True:
+        # generate a candidate pseudo-mersenne prime
         p = 2**bits - (secrets.randbelow((0xFFFFFFFFFFFF+0x24)&((1<<32)-1))+1)
-        if ((math.factorial(p - 1) % p) + 1) == p:
+
+        # check if the candidate is prime using wilson's theoreom
+        if ((factorial(p - 1) % p) + 1) == p:
             return p
 
 p = make_prime(414)
@@ -13,8 +21,11 @@ q = make_prime(414)
 n = p * q
 t = (p - 1) * (q - 1)
 e = 65537
-d = pow(e, -1, t)
+
+print(f"n = {n}")
+print(f"e = {e}")
 
 m = int.from_bytes(flag.FLAG, byteorder="big")
 c = pow(m, e, n)
-print(c)
+
+print(f"c = {c}")

